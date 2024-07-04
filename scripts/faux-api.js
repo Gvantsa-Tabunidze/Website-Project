90db3fb251ac528159d73e22a1bc9c9b1031bc88
Gvantsa-Tabunidze <gvantsaletta9@gmail.com>
2024-07-04 13:42:40 +0400

    add

--- scripts/faux-api.js
+++ scripts/faux-api.js
@@ -9,17 +9,25 @@ export function getProductInfo() {
     .get('https://faux-api.com/api/v1/honeyinfo_7192215413888481')
     .then(function (response) {
       console.log(response.data.result);
+      
+      const fragment = new DocumentFragment();
+
       response.data.result.forEach((element) => {
         const productCard = document.createElement('li');
+        productCard.classList.add('productCard');
         const itemImage = document.createElement('img');
         itemImage.src = element.image;
         itemImage.classList.add('itemImage');
-        productCard.appendChild(itemImage);
-        productCard.classList.add('productImageDiv');
         const heading = document.createElement('h3');
         heading.innerText = element.name;
+        
+
+        productCard.appendChild(itemImage);      
         productCard.appendChild(heading);
-        ulHoney.appendChild(productCard);
+        fragment.appendChild(productCard);
+       
+        ulHoney.appendChild(fragment);
+        
       });
     })
     .catch(function (error) {
